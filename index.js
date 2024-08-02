14aff732033177a26136599e720b276f1f664638
Nthabiseng Mashalane <nthabimashalane0@gmail.com>
2024-08-02 12:54:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
              remove } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"
 
     const firebaseConfig = { 
-        databaseURL: "https://lead-tracker-app-4a722-default-rtdb.europe-west1.firebasedatabase.app/"
+        databaseURL:"https://lead-tracker-app-4a722-default-rtdb.europe-west1.firebasedatabase.app/"
     }
 
     const app = initializeApp(firebaseConfig)
